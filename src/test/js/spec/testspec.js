6655ac401b7ed8211a03e84e63506718ff5114a2
unknown <vijay_rajput@D-CL152R1.us.pjids.net>
2014-07-01 17:32:43 -0400

    improved version

--- src/test/js/spec/testspec.js
+++ src/test/js/spec/testspec.js
@@ -1,32 +1,31 @@
-
-  describe("test", function() {
-    beforeEach(function() {
-      var path = '';
+describe("test", function () {
+    beforeEach(function () {
+        var path = '';
         if (typeof window.__karma__ !== 'undefined') {
-          path += 'base/'
-        } 
-        jasmine.getFixtures().fixturesPath =  path + 'test/fixtures/';
-       // jasmine.getStyleFixtures().fixturesPath = path + 'test/fixtures/';
-      //  loadStyleFixtures('mycssfixture.css');
-        loadFixtures('myfixture.html');
-        if ( typeof ( console ) !== 'undefined' && console != null) {
-            console.log("Log exists!");
+            path += 'base/'
         }
-        else {
+        jasmine.getFixtures().fixturesPath = path + 'test/fixtures/';
+        // jasmine.getStyleFixtures().fixturesPath = path + 'test/fixtures/';
+        //  loadStyleFixtures('mycssfixture.css');
+        loadFixtures('myfixture.html');
+        if (typeof ( console ) === 'undefined' || console == null) {
             console = {
-                log: function () {},
-                warn: function () {},
-                error: function () {}
+                log: function () {
+                },
+                warn: function () {
+                },
+                error: function () {
+                }
             }
         }
     });
-    it("test", function() {
-      var returnVal = fun1();
-      expect(returnVal).toEqual(5);
+    it("test", function () {
+        var returnVal = fun1();
+        expect(returnVal).toEqual(5);
     });
-    it('test height',function(){
-       var divHeight = checkHeight();
-       expect(divHeight).toEqual(500);
+    it('test height', function () {
+        var divHeight = checkHeight();
+        expect(divHeight).toEqual(500);
 
     });
 });
